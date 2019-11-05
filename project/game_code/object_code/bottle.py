cc7b883c777c17b2cd46a6bb60cb914b73682172
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2019-11-05 22:26:18 +0900

    보스 패턴 구현
    페이즈 1,2 구현

--- project/game_code/object_code/bottle.py
+++ project/game_code/object_code/bottle.py
@@ -2,6 +2,8 @@ from pico2d import *
 from math import sin, cos
 from project.game_code.object_code import game_world
 
+pi = 3.14159256
+
 
 class Bottle:
     image = None
@@ -15,16 +17,16 @@ class Bottle:
         self.velocity = 0.5 * phase
 
     def draw(self):
-        self.image.clip_draw(self.frame * 0, 0, 0, 0, self.x, self.y, 40, 40)
+        self.image.clip_draw(self.frame * 12 + 2, 0, 11, 14, self.x, self.y, 40, 40)
 
     def update(self):
         self.frame_speed_control += 1
-        if self.frame_speed_control > (60-self.phase*10):
+        if self.frame_speed_control > (60 - self.phase * 10):
             self.frame = (self.frame + 1) % 4
             self.frame_speed_control = 0
 
-        self.x += 1
-        self.y += self.y * cos(self.dir)
+        self.x += math.sin(self.dir * pi / 180)
+        self.y += math.cos(self.dir * pi / 180)
 
         if self.x < 0 or self.x > 960 or self.y < 0 or self.y > 550:
             game_world.remove_object(self)
--- project/game_code/object_code/drunk.py
+++ project/game_code/object_code/drunk.py
@@ -1,22 +1,29 @@
 from pico2d import *
 import random
+import math
 from project.game_code.state_code import main_state
 from project.game_code.object_code import game_world
 from project.game_code.object_code.bottle import Bottle
 
+pi = 3.14159256
+dragon_x = 480
+dragon_y = 300
+
 
 class Drunk:
-    def __init__(self, x=480, y=300):
+    def __init__(self, x=dragon_x, y=dragon_x):
         self.x, self.y = x, y
         self.frame = 0
         self.dir = 1
         self.frame_speed_control = 0
-        self.phase = 1
+        self.phase = 2
         self.is_hit = False
         self.image = load_image('sprite\\Enemy\\boss.png')
         self.hp = 200
         self.bottle_number = 0
         self.attack_timer = 0
+        self.radius = 20
+        self.angle = 0
 
     def update(self):
         self.frame_speed_control += 1
@@ -25,17 +32,32 @@ class Drunk:
             self.frame = (self.frame + 1) % 8
             self.frame_speed_control = 0
 
-        if self.hp >= 150:
-            self.phase = 1
-        elif self.hp >= 60:
-            self.phase = 2
-        else:
-            self.phase = 3
+#        if self.hp >= 150:
+#            self.phase = 1
+#        elif self.hp >= 60:
+#            self.phase = 2
+#        else:
+#           self.phase = 3
 
         if self.attack_timer > 100 - (self.phase * 15):
-            Bottle(self.x, self.y, self.phase, self.bottle_number)
+            self.bottle()
             self.bottle_number = (self.bottle_number + 1) % 8
             self.attack_timer = 0
+
+        if self.phase == 1:
+            if self.dir == 1:
+                self.x += 0.5
+                if self.x == 660:
+                    self.dir = -1
+            else:
+                self.x -= 0.5
+                if self.x == 300:
+                    self.dir = 1
+
+        elif self.phase == 2:
+            self.angle += 1
+            self.x += self.radius * math.cos(self.angle * pi / 180) + dragon_x
+            self.y += self.radius * math.sin(self.angle * pi / 180) + dragon_y
 
     def draw(self):
         if self.is_hit:
@@ -45,9 +67,8 @@ class Drunk:
             if self.dir == 1:
                 self.image.clip_draw(self.frame * 64, 512, 64, 64, self.x, self.y, 200, 200)
             else:
-                self.image.clip_draw(self.frame * 64, 576, 32, 32, self.x, self.y, 200, 200)
+                self.image.clip_draw(self.frame * 64, 576, 64, 64, self.x, self.y, 200, 200)
 
-#  def bottle(self):
-#      bottle = Bottle(self.x, self.y, self.phase, self.bottle_number)
-#    game_world.add_object(bottle, 3)
-#     self.bottle_number = (self.bottle_number + 1) % 8
+    def bottle(self):
+        bottle = Bottle(self.x, self.y, self.phase, self.bottle_number)
+        game_world.add_object(bottle, 1)
